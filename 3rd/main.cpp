620eb0f5de90e2a84323813ddb27f391ac98621d
UncNunkie <new.677.mail@gmail.com>
2023-03-27 18:46:16 +0200

    3rd: moo indents

--- 3rd/main.cpp
+++ 3rd/main.cpp
@@ -4,26 +4,27 @@
 #include <map>
 
 int main() {
-  std::map<int, int, std::less<>,
-           fixed_size_allocator<std::pair<const int, int>, 10>>
-      m;
-  for (int i = 0; i < 10; ++i) {
-    std::cout << " i = " << i << std::endl;
-    m[i] = i;
-    std::cout << "m[i] = " << m[i] << " ptr=" << std::hex << uint64_t(&m[i])
-              << std::dec << std::endl;
-  }
+    std::map<int, int, std::less<>,
+             fixed_size_allocator<std::pair<const int, int>, 10>>
+        m;
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << " i = " << i << std::endl;
+        m[i] = i;
+        std::cout << "m[i] = " << m[i] << " ptr=" << std::hex << uint64_t(&m[i])
+                << std::dec << std::endl;
+    }
 
-  for (auto &[k, v] : m) {
-    std::cout << "m[" << k << "]=" << v << std::endl;
-  }
+    for (auto &[k, v] : m) {
+        std::cout << "m[" << k << "]=" << v << std::endl;
+    }
 
-  flat_map<int, int, custom_allocator_2<std::pair<int, int>>> m1;
-  for (int i = 0; i < 10; ++i) {
-    m1[i] = i;
-  }
+    flat_map<int, int, custom_allocator_2<std::pair<int, int>>> m1;
+    for (int i = 0; i < 10; ++i) {
+        m1[i] = i;
+    }
 
-  for (int i = 0; i < 10; ++i) {
-    std::cout << "RD: m1[" << i << "] = " << m1[i] << std::endl;
-  }
+    for (int i = 0; i < 10; ++i) {
+        std::cout << "RD: m1[" << i << "] = " << m1[i] << std::endl;
+    }
 }
